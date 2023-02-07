5705c40468cdb6ee16b4bbd48fb6680019e915ac
Mansi Sonawani <12345mansis@gmail.com>
2023-02-07 22:39:34 +0530

    assets

---
+++ client/kodesprint/src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+   
+    <div class = "left">
+      <ul class = "nav">
+        <li><a href="!#" >Home</a></li>
+        <li><a href="!#">search</a></li>
+        <li><a href="!#">To Do List</a></li>
+        <li><a href="!#">Git</a></li>
+        <li><a href="!#">AI</a></li>
+        <li><a href="!#">My profile</a></li>
+      </ul>
+    </div>
+
+  
+);
+
+}
+export default App;
